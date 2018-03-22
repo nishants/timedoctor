c592af97ceb5b44bbef6edab645968c39a65bb7b
nishant <nishant.singh87@gmail.com>
2018-03-22 12:55:56 +0530

    use timer reducer.

--- app/routes.js
+++ app/routes.js
@@ -2,14 +2,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
 import App from './containers/App';
-import HomePage from './containers/HomePage';
 import CounterPage from './containers/CounterPage';
+import MainWindow from './main-window/main-window-component';
+import TimerListener from './timer/timer-listerner-component';
 
 export default () => (
   <App>
     <Switch>
       <Route path="/counter" component={CounterPage} />
-      <Route path="/" component={HomePage} />
+      <Route path="/" component={MainWindow} />
     </Switch>
+    <TimerListener />
   </App>
 );
